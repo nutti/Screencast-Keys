1b15b3c3592e8b8588b184c6b69e751675117ecd
nutti <nutti.metro@gmail.com>
2018-10-07 09:24:07 +0900

    Add: install tool

---
+++ tools/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+    echo "Usage: tools/install.sh <os> <version>"
+fi
+
+os=${1}
+version=${2}
+target=""
+
+if [ ${os} = "mac" ]; then
+    target="${HOME}/Library/Application Support/Blender/${version}/scripts/addons/screencastkeys"
+elif [ ${os} = "linux" ]; then
+    target="${HOME}/.config/blender/${version}/scripts/addons/screencastkeys"
+else
+    echo "Invalid operating system."
+    exit 1
+fi
+
+rm -rf "${target}"
+cp -r screencastkeys "${target}"
