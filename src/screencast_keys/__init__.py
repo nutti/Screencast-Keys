716e51d36c5e3df13e8e214034ecbe616614fb6d
nutti <nutti.metro@gmail.com>
2021-08-29 15:22:08 +0900

    Update: Author of the add-on

--- src/screencast_keys/__init__.py
+++ src/screencast_keys/__init__.py
@@ -22,7 +22,8 @@
 bl_info = {
     "name": "Screencast Keys",
     "author": "Paulo Gomes, Bart Crouch, John E. Herrenyo, "
-              "Gaia Clary, Pablo Vazquez, chromoly, Nutti, Hawkpath",
+              "Gaia Clary, Pablo Vazquez, chromoly, Nutti, Hawkpath, "
+              "CheeryLee, Kureii",
     "version": (3, 5, 0),
     "blender": (2, 80, 0),
     "location": "3D View > Sidebar > Screencast Keys",
